a764a7f5ed567f9fd123da47d7fc2ecde4903dcc
Ankit <ankit@idealake.com>
2024-02-27 23:09:00 +0530

    Multifield through Java code committetd

---
+++ Idealake/core/src/main/java/com/Idealake/core/models/NumberCardsModel.java
@@ -0,0 +1,30 @@
+package com.Idealake.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
+
+import javax.inject.Inject;
+
+
+@Model(adaptables = Resource.class)
+public class NumberCardsModel {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NumberCardsModel.class);
+
+    @Inject
+    @Optional
+    private String title;
+
+    @Inject
+    @Optional
+    public Resource numbercards;
+
+    public String getTitle() {
+        return title;
+    }
+
+}
