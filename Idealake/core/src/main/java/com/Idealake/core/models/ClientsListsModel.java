27502762e08a14c0800efd93deab0493725a703b
Monika Raturi <monika_raturi@ildc-inlp.idealake.com>
2024-03-01 17:04:13 +0530

    changes added

---
+++ Idealake/core/src/main/java/com/Idealake/core/models/ClientsListsModel.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2015 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.Idealake.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+
+import javax.inject.Inject;
+
+@Model(adaptables=Resource.class)
+public class ClientsListsModel {
+
+    @Inject
+    @Optional
+    public Resource products1;
+    @Inject
+    @Optional
+    public Resource products2;
+    @Inject
+    @Optional
+    public Resource products3;
+    @Inject
+    @Optional
+    public Resource products4;
+    @Inject
+    @Optional
+    public Resource products5;
+    @Inject
+    @Optional
+    public Resource products6;
+
+}
+
