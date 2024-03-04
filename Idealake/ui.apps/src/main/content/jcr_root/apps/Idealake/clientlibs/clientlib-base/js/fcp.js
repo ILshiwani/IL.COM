fb8604c66ad4ed356ce85d60d56648ee191a308c
alphaScorpi <vinay.bommena99@gmail.com>
2024-03-04 15:30:20 +0530

    deleted components

--- Idealake/ui.apps/src/main/content/jcr_root/apps/Idealake/clientlibs/clientlib-base/js/fcp.js
+++ Idealake/ui.apps/src/main/content/jcr_root/apps/Idealake/clientlibs/clientlib-base/js/fcp.js
@@ -1,47 +1,70 @@
 function lazyLoadImgs() {
     var t = [];
-
     function e() {
-        for (var e = 0; e < t.length; e++) n = t[e], o = void 0, (o = n.getBoundingClientRect()).bottom >= 0 && o.right >= 0 && o.top <= (window.innerHeight || document.documentElement.clientHeight) && o.left <= (window.innerWidth || document.documentElement.clientWidth) && t[e].getAttribute("data-src") && (t[e].src = t[e].getAttribute("data-src"), t[e].removeAttribute("data-src"));
+        for (var e = 0; e < t.length; e++)
+            n = t[e],
+            o = void 0,
+            (o = n.getBoundingClientRect()).bottom >= 0 && o.right >= 0 && o.top <= (window.innerHeight || document.documentElement.clientHeight) && o.left <= (window.innerWidth || document.documentElement.clientWidth) && t[e].getAttribute("data-src") && (t[e].src = t[e].getAttribute("data-src"),
+            t[e].removeAttribute("data-src"));
         var n, o;
         t = Array.prototype.filter.call(t, (function(t) {
             return t.getAttribute("data-src")
-        }))
+        }
+        ))
     }
-
     function n(t, e) {
         window.addEventListener ? window.addEventListener(t, e) : window.attachEvent("on" + t, e)
     }
     n("load", (function() {
         t = document.querySelectorAll(".lazy")
-    })), n("load", e), n("scroll", e), n("resize", e), n("click", e)
+    }
+    )),
+    n("load", e),
+    n("scroll", e),
+    n("resize", e),
+    n("click", e)
 }
-
 function Menu() {
     $(window).width() > 767 && $(window).scroll((function() {
         $(window).scrollTop() >= 50 ? $("header").addClass("headmenu") : $("header").removeClass("headmenu")
-    })), $(window).width() < 991 && $("header").addClass("headmenu"), $(document).on("click", ".js-mobileMenu", (function() {
-        $(this).toggleClass("open"), $(".nav-list").toggleClass("navaddbox")
-    }))
+    }
+    )),
+    $(window).width() < 991 && $("header").addClass("headmenu"),
+    $(document).on("click", ".js-mobileMenu", (function() {
+        $(this).toggleClass("open"),
+        $(".nav-list").toggleClass("navaddbox")
+    }
+    ))
 }
-
 function hamburger() {
     $(".humburg-menu").click((function() {
-        $(this).toggleClass("open"), $(".nav-list").toggleClass("active"), $(".cm-overlay").toggleClass("active"), $(".search-wrap").toggleClass("active")
-    }))
+        $(this).toggleClass("open"),
+        $(".nav-list").toggleClass("active"),
+        $(".cm-overlay").toggleClass("active"),
+        $(".search-wrap").toggleClass("active")
+    }
+    ))
 }
 $(document).ready((function() {
-    hamburger(), AOS.init({
+    hamburger(),
+    AOS.init({
         duration: 2e3,
         once: !0,
         disable: "mobile",
         offset: 150
     })
-})), $(window).on("load", (function() {
+}
+)),
+$(window).on("load", (function() {
     setTimeout((function() {
         $("#loader").fadeOut(500)
-    }), 10)
-})), $(window).resize((function() {
+    }
+    ), 10)
+}
+)),
+$(window).resize((function() {
     winWidth,
     winWidth
-})), lazyLoadImgs();
+}
+)),
+lazyLoadImgs();
--- Idealake/ui.apps/src/main/content/jcr_root/apps/Idealake/clientlibs/clientlib-base/js/global.js
+++ Idealake/ui.apps/src/main/content/jcr_root/apps/Idealake/clientlibs/clientlib-base/js/global.js
@@ -1,37 +1,57 @@
-var winWidth = $(window).width(),
-    winHeight = $(window).height();
-
+var winWidth = $(window).width()
+  , winHeight = $(window).height();
 function showscrollDiv() {
     $(this).scrollTop() > $(window).height() - $("header").outerHeight() ? $(".js_scrollTop").fadeIn() : $(".js_scrollTop").fadeOut()
 }
-
 function scrolltoTop() {
     $(document).on("click", ".js_scrollTop", (function() {
         return $("html, body").animate({
             scrollTop: 0
-        }, "slow"), !1
-    }))
+        }, "slow"),
+        !1
+    }
+    ))
 }
-
 function Tab() {
     $(document).on("click", ".bs-tab .nav-item", (function() {
-        $(this).parents(".nav-tabs").find(".nav-item").removeClass("active"), $(this).addClass("active");
+        $(this).parents(".nav-tabs").find(".nav-item").removeClass("active"),
+        $(this).addClass("active");
         var e = $(this).index();
-        $(this).parents(".bs-tab").find(".tab-pane").removeClass("active"), $(this).parents(".bs-tab").find(".tab-pane").eq(e).addClass("active")
-    }))
+        $(this).parents(".bs-tab").find(".tab-pane").removeClass("active"),
+        $(this).parents(".bs-tab").find(".tab-pane").eq(e).addClass("active")
+    }
+    ))
 }
 $(document).ready((function() {
-    showscrollDiv(), scrolltoTop(), Tab(), winWidth <= 991 && $(".accordin-head").on("click", (function() {
-        $(this).parents(".acc-cont").siblings(".acc-cont").find(".accordin-head").next(".accordin-content").slideUp(), $(this).parents(".acc-cont").siblings(".acc-cont").find(".accordin-head").removeClass("active"), $(this).siblings(".accordin-head").removeClass("active"), $(this).next().slideToggle(), $(this).siblings(".accordin-head").removeClass("active"), $(this).toggleClass("active")
-    }))
-})), $(window).on("load", (function() {
+    showscrollDiv(),
+    scrolltoTop(),
+    Tab(),
+    winWidth <= 991 && $(".accordin-head").on("click", (function() {
+        $(this).parents(".acc-cont").siblings(".acc-cont").find(".accordin-head").next(".accordin-content").slideUp(),
+        $(this).parents(".acc-cont").siblings(".acc-cont").find(".accordin-head").removeClass("active"),
+        $(this).siblings(".accordin-head").removeClass("active"),
+        $(this).next().slideToggle(),
+        $(this).siblings(".accordin-head").removeClass("active"),
+        $(this).toggleClass("active")
+    }
+    ))
+}
+)),
+$(window).on("load", (function() {
     setTimeout((function() {
         $(window).scrollTop(0)
-    }), 100)
-}));
-const burgerMenu = document.getElementById("burger"),
-    navbarMenu = document.getElementById("menu"),
-    overlay = document.getElementById("md-layer");
-burgerMenu.addEventListener("click", (() => {
-    burgerMenu.classList.toggle("is-active"), overlay.classList.toggle("is-active"), navbarMenu.classList.toggle("is-active"), navbarMenu.classList.contains("is-active") ? navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px" : navbarMenu.removeAttribute("style")
-})), $("h3").addClass("animated fadeIn");
+    }
+    ), 100)
+}
+));
+const burgerMenu = document.getElementById("burger")
+  , navbarMenu = document.getElementById("menu")
+  , overlay = document.getElementById("md-layer");
+burgerMenu.addEventListener("click", (()=>{
+    burgerMenu.classList.toggle("is-active"),
+    overlay.classList.toggle("is-active"),
+    navbarMenu.classList.toggle("is-active"),
+    navbarMenu.classList.contains("is-active") ? navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px" : navbarMenu.removeAttribute("style")
+}
+)),
+$("h3").addClass("animated fadeIn");
