539352ffae787c081e0b2ca6bd1fa22a28952546
alphaScorpi <vinay.bommena99@gmail.com>
2024-04-17 12:05:31 +0530

    CTA banner variation Vinay

---
+++ Idealake/core/src/main/java/com/Idealake/core/models/Banner.java
@@ -0,0 +1,16 @@
+package com.Idealake.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class)
+public class Banner {
+
+    @Inject
+    @Optional
+    public Resource type;
+
+}
